5a8476b41f83f9d51f0edf356785a3794b213d3f
sajadgilga <majidstic@yahoo.com>
2018-11-29 15:06:48 +0330

    added oobject model

--- Bigher/settings.py
+++ Bigher/settings.py
@@ -12,6 +12,16 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from django.core.exceptions import ImproperlyConfigured
+
+
+def get_env_variable(var_name):
+    try:
+        return os.environ[var_name]
+    except KeyError:
+        error_msg = "Set the %s environment variable" % var_name
+        raise ImproperlyConfigured(error_msg)
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -75,9 +85,22 @@ WSGI_APPLICATION = 'Bigher.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.path.join(BASE_DIR, 'db.postgresdb'),
+    #     # 'NAME': get_env_variable('bigheri'),
+    #     # 'USER': get_env_variable('bigher'),
+    #     # 'PASSWORD': get_env_variable('bigher'),
+    #     # 'HOST': '127.0.0.1',
+    #     # 'PORT': '8080',
+    # }
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.postgresdb'),
+        'NAME': 'bagheri',
+        'USER': 'bagher',
+        'PASSWORD': '1234',
+        'HOST': '127.0.0.1',
+        'PORT': '',
     }
 }
 
